7b791e6332d4b6118c6b4b08624fddebadc6f88c
BattleShip Park <battleship.park@gmail.com>
2017-04-27 14:47:33 +0900

    Add rome-modules to get image by parsing itunes tag

--- app/src/androidTest/java/com/battleshippark/bsp_langpod/MainActivityTest.java
+++ app/src/androidTest/java/com/battleshippark/bsp_langpod/MainActivityTest.java
@@ -2,6 +2,8 @@ package com.battleshippark.bsp_langpod;
 
 import android.support.test.rule.ActivityTestRule;
 
+import com.rometools.modules.itunes.FeedInformation;
+import com.rometools.rome.feed.module.Module;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
@@ -23,6 +25,9 @@ public class MainActivityTest {
     public void rome() throws IOException, FeedException {
         String url = "http://enabler.kbs.co.kr/api/podcast_channel/feed.xml?channel_id=R2017-0027";
         SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
-        System.out.println(feed.getTitle());
+
+        Module module = feed.getModule("http://www.itunes.com/dtds/podcast-1.0.dtd");
+        FeedInformation feedInfo = (FeedInformation) module;
+        System.out.println(feedInfo.getImage());
     }
 }
