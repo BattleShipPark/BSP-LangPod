f31ef172cb78e5ac5949f5f22f3aabb0cd655340
BattleShip Park <battleship.park@gmail.com>
2017-06-14 15:40:59 +0900

    Upgrade gradle plugin. Use only notifyDataSetChanged() when data is changed because items blink.

--- app/src/main/java/com/battleshippark/bsp_langpod/presentation/RealmRecyclerViewAdapter.java
+++ app/src/main/java/com/battleshippark/bsp_langpod/presentation/RealmRecyclerViewAdapter.java
@@ -52,31 +52,7 @@ public abstract class RealmRecyclerViewAdapter<T extends RealmModel, S extends R
         return new OrderedRealmCollectionChangeListener() {
             @Override
             public void onChange(Object collection, OrderedCollectionChangeSet changeSet) {
-                // null Changes means the async query returns the first time.
-                if (changeSet == null) {
-                    notifyDataSetChanged();
-                    return;
-                }
-                // For deletions, the adapter has to be notified in reverse order.
-                OrderedCollectionChangeSet.Range[] deletions = changeSet.getDeletionRanges();
-                for (int i = deletions.length - 1; i >= 0; i--) {
-                    OrderedCollectionChangeSet.Range range = deletions[i];
-                    notifyItemRangeRemoved(range.startIndex, range.length);
-                }
-
-                OrderedCollectionChangeSet.Range[] insertions = changeSet.getInsertionRanges();
-                for (OrderedCollectionChangeSet.Range range : insertions) {
-                    notifyItemRangeInserted(range.startIndex, range.length);
-                }
-
-                if (!updateOnModification) {
-                    return;
-                }
-
-                OrderedCollectionChangeSet.Range[] modifications = changeSet.getChangeRanges();
-                for (OrderedCollectionChangeSet.Range range : modifications) {
-                    notifyItemRangeChanged(range.startIndex, range.length);
-                }
+                notifyDataSetChanged();
             }
         };
     }
