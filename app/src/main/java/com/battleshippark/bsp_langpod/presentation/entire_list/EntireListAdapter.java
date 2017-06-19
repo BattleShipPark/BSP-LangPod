991943623e2dad28b08ce5f2bfaddeda610aa0df
BattleShip Park <battleship.park@gmail.com>
2017-06-19 21:33:08 +0900

    Adjust UI layout in entire channel list

--- app/src/main/java/com/battleshippark/bsp_langpod/presentation/entire_list/EntireListAdapter.java
+++ app/src/main/java/com/battleshippark/bsp_langpod/presentation/entire_list/EntireListAdapter.java
@@ -4,12 +4,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.battleshippark.bsp_langpod.R;
 import com.battleshippark.bsp_langpod.data.db.ChannelRealm;
 import com.battleshippark.bsp_langpod.presentation.RealmRecyclerViewAdapter;
 import com.battleshippark.bsp_langpod.presentation.entire_list.EntireListFragment.OnListFragmentInteractionListener;
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,30 +35,27 @@ class EntireListAdapter extends RealmRecyclerViewAdapter<ChannelRealm, EntireLis
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mIdView.setText(String.valueOf(getData().get(position).getId()));
-        holder.mTitleView.setText(getData().get(position).getTitle());
+        ChannelRealm channelRealm = getData().get(position);
 
+        holder.titleView.setText(channelRealm.getTitle());
+
+        Glide.with(holder.imageView.getContext()).load(channelRealm.getImage()).into(holder.imageView);
         holder.itemView.setOnClickListener(v -> {
             if (null != mListener) {
-                mListener.onListFragmentInteraction(getData().get(position));
+                mListener.onListFragmentInteraction(channelRealm);
             }
         });
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.id)
-        TextView mIdView;
         @BindView(R.id.title_tv)
-        TextView mTitleView;
+        TextView titleView;
+        @BindView(R.id.image_iv)
+        ImageView imageView;
 
         ViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mTitleView.getText() + "'";
         }
     }
 }
