39a0c1b80b9d22a82d7d1722121d60e6aba3eed9
BattleShip Park <battleship.park@gmail.com>
2017-10-13 11:31:58 +0900

    Add ToolBar

--- app/src/main/java/com/battleshippark/bsp_langpod/presentation/setting/SettingDownloadListActivity.java
+++ app/src/main/java/com/battleshippark/bsp_langpod/presentation/setting/SettingDownloadListActivity.java
@@ -1,15 +1,50 @@
 package com.battleshippark.bsp_langpod.presentation.setting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.battleshippark.bsp_langpod.R;
 
-public class SettingDownloadListActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class SettingDownloadListActivity extends Activity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.channel_rv)
+    RecyclerView channelRv;
+    @BindView(R.id.msg_tv)
+    TextView msgTv;
+    private Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_download_list);
+
+        unbinder = ButterKnife.bind(this);
+
+        initData(savedInstanceState);
+        initUI();
+    }
+
+    private void initUI() {
+        setActionBar(toolbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void initData(Bundle savedInstanceState) {
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
     }
 }
