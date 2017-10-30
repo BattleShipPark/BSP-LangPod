30deea7266ec174ce890f0723b292f3f3a31c462
BattleShip Park <battleship.park@gmail.com>
2017-10-30 20:30:54 +0900

    Use DownloaderBroadcastReceiver in SettingDownloadListActivity

--- app/src/main/java/com/battleshippark/bsp_langpod/presentation/setting/SettingDownloadListActivity.java
+++ app/src/main/java/com/battleshippark/bsp_langpod/presentation/setting/SettingDownloadListActivity.java
@@ -9,14 +9,22 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.annimon.stream.Optional;
+import com.annimon.stream.Stream;
 import com.battleshippark.bsp_langpod.R;
 import com.battleshippark.bsp_langpod.dagger.DaggerDbApiGraph;
 import com.battleshippark.bsp_langpod.data.db.DownloadRealm;
 import com.battleshippark.bsp_langpod.data.db.EpisodeRealm;
+import com.battleshippark.bsp_langpod.data.downloader.DownloadCompleteParam;
+import com.battleshippark.bsp_langpod.data.downloader.DownloadErrorParam;
+import com.battleshippark.bsp_langpod.data.downloader.DownloadProgressParam;
 import com.battleshippark.bsp_langpod.domain.GetDownloadList;
 import com.battleshippark.bsp_langpod.presentation.EpisodeDateFormat;
+import com.battleshippark.bsp_langpod.service.downloader.DownloaderBroadcastReceiver;
+import com.battleshippark.bsp_langpod.util.Logger;
 import com.bumptech.glide.Glide;
 
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
@@ -37,12 +45,15 @@ public class SettingDownloadListActivity extends Activity implements OnItemListe
     @BindView(R.id.msg_tv)
     TextView msgTv;
 
+    private static final String TAG = SettingDownloadListActivity.class.getSimpleName();
+    private static final Logger logger = new Logger(TAG);
+    private final CompositeSubscription subscription = new CompositeSubscription();
+    private final EpisodeDateFormat dateFormat = new EpisodeDateFormat();
     private Unbinder unbinder;
     private DownloadListAdapter adapter;
     private GetDownloadList getDownloadList;
-    private final CompositeSubscription subscription = new CompositeSubscription();
-
-    private final EpisodeDateFormat dateFormat = new EpisodeDateFormat();
+    private DownloaderBroadcastReceiver downloaderBcReceiver;
+    private List<DownloadRealm> downloadRealmList = Collections.EMPTY_LIST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,15 +86,67 @@ public class SettingDownloadListActivity extends Activity implements OnItemListe
         adapter = new DownloadListAdapter(this);
         getDownloadList = new GetDownloadList(
                 DaggerDbApiGraph.create().downloadApi(), Schedulers.computation(), AndroidSchedulers.mainThread());
+        downloaderBcReceiver = new DownloaderBroadcastReceiver(this,
+                this::onDownloadProgress, this::onDownloadCompleted, this::onDownloadError);
+    }
+
+    private void onDownloadProgress(DownloadProgressParam param) {
+        findDownload(param.identifier())
+                .ifPresent(downloadRealm -> {
+                    downloadRealm.setDownloadState(DownloadRealm.DownloadState.DOWNLOADING);
+                    downloadRealm.getEpisodeRealm().setDownloadedBytes(param.bytesRead());
+                    downloadRealm.getEpisodeRealm().setTotalBytes(param.contentLength());
+                    adapter.notifyDataSetChanged();
+                    logger.d("episode=%d, bytes=%d, total=%d", downloadRealm.getId(), param.bytesRead(), param.contentLength());
+                });
+    }
+
+    private void onDownloadCompleted(DownloadCompleteParam param) {
+        findDownload(param.identifier())
+                .ifPresent(downloadRealm -> {
+                    downloadRealm.setDownloadState(DownloadRealm.DownloadState.DOWNLOADED);
+                    downloadRealm.getEpisodeRealm().setDownloadedPath(param.file().getAbsolutePath());
+                    adapter.notifyDataSetChanged();
+                    logger.d("episode=%d completed", downloadRealm.getId());
+                });
+    }
+
+    private void onDownloadError(DownloadErrorParam param) {
+        findDownload(param.identifier())
+                .ifPresent(downloadRealm -> {
+                    downloadRealm.setDownloadState(DownloadRealm.DownloadState.FAILED_DOWNLOAD);
+                    adapter.notifyDataSetChanged();
+                    logger.d("episode=%d error", downloadRealm.getId());
+                });
+        logger.w(param.throwable());
+    }
+
+    private Optional<DownloadRealm> findDownload(String episodeId) {
+        return Stream.of(downloadRealmList)
+                .filter(downloadRealm -> downloadRealm.getEpisodeId() == Long.valueOf(episodeId))
+                .findFirst();
     }
 
     private void showData(List<DownloadRealm> downloadRealms) {
+        this.downloadRealmList = downloadRealms;
         adapter.setData(downloadRealms);
         if (downloadRealms.isEmpty()) {
             msgTv.setVisibility(View.VISIBLE);
         } else {
             msgTv.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        downloaderBcReceiver.register();
+    }
+
+    @Override
+    protected void onStop() {
+        downloaderBcReceiver.unregister();
+        super.onStop();
     }
 
     @Override
